c94f3120a34345eda22261fa465efc7203a6f1b8
singha5 <arunjitsingh6@gmail.com>
2014-05-01 09:09:22 -0400

    sync

---
+++ consecutive integers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            for(i = 42; i < 96 ; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
